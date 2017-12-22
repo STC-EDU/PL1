339f410aa247d4e0775ab6a14209cc884c3be829
STC-EDU <hosaka@suntech.jp>
2017-12-22 10:31:29 +0900

    Create p1.c

---
+++ 1/p1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void)
+{
+    printf("Nice to meet you\n");
+    printf("My name is ______.\n");
+    printf("I started programming today.\n");
+    
+    printf("はじめまして！\n");
+    printf("____ と言います。\n");
+    printf("今日からプログラミングをはじめました。");
+    
+    return 0;
+}
